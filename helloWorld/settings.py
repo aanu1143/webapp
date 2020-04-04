05e5199f5d1b1887ba61bd56b59eae12ae1fb82d
Anand Biswas <41153602+aanu1143@users.noreply.github.com>
2020-04-04 23:18:58 +0530

    final

--- helloWorld/settings.py
+++ helloWorld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')$h-=bn@)!s@(djz#dsh$biwd3s9d$d^rbkp(fnyy=^b85u0)j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
